15470ca3cf2d715d2a146d5a7de95e2d5be518f1
Brian Kim <bk@delphoslabs.com>
2025-02-01 22:26:03 -0500

    use mongo backend

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, jsonify
+from pymongo import MongoClient
 
+client = MongoClient("mongodb://admin:secret@localhost:27017/")
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return 'hello'
 
+"""
 route_map = {
     'knicks': 'https://www.espn.com/nba/team/_/name/ny/new-york-knicks',
     'chevy': 'https://www.chevrolet.com/?cmp=OLA_DISPLAY_33145653_413137889_606392551_226395378&gclid=EAIaIQobChMIu_ylgbOjiwMVpa6DCB0ckRFMEAEYASAAEgIdhfD_BwE',
 }
+"""
 
 @app.route('/l', methods=["GET"])
 def get_redirect():
@@ -18,12 +22,17 @@ def get_redirect():
 @app.route('/l', methods=["POST"])
 def create_redirect():
     data = request.get_json()
-    route_map[data["short"]] = data["long"]
+    # route_map[data["short"]] = data["long"]
+    existing = client.db.links.find_one({"short": data["short"]}) # could we avoid this with an index?
+    if existing is not None:
+        return jsonify({"error": "short link already exists"}), 409
+    response = client.db.links.insert_one({"long": data["long"], "short": data["short"]})
     return data
 
-@app.route('/l/<link>')
+@app.route('/l/<link>', methods=["GET"])
 def redirect_link(link):
-    new_link = route_map[link]
-    return redirect(new_link)
+    # new_link = route_map[link]
+    data = client.db.links.find_one({"short": link})
+    return redirect(data["long"])
 
 app.run(port=2000, debug=True)
